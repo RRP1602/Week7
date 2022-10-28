901a991857d3b7451e2e8399a22279913386f003
Rahul Patel <rp521@kent.ac.uk>
2022-10-28 11:13:49 +0100

    set up for mvc page done

---
+++ SpartaToDo/SpartaToDo/Data/Migrations/20221028094233_all.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpartaToDo.Data.Migrations
+{
+    public partial class all : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SpartaToDo/SpartaToDo/Data/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpartaToDo.Models;
+
+namespace SpartaToDo.Data
+{
+    public class SeedData
+    {
+
+        public static void Initialise(IServiceProvider serviceProvider)
+        {
+            using (var db = new SpartaToDoContext(serviceProvider.GetRequiredService<DbContextOptions<SpartaToDoContext>>()))
+            {
+                if(db.ToDos.Any())
+                {
+                    return; //Db already has data
+                }
+
+                db.ToDos.AddRange
+                    (
+                    new ToDo
+
+                    {
+                        Title = " Sleep",
+                        Description = " Go to Bed early",
+                        Complete = true,
+                        Date= new DateTime(2022,10,20)
+
+                    },
+                    new ToDo
+                    {
+                        Title = "Teach C#",
+                        Description = "Teach the class",
+                        Complete = true
+
+                    }
+                );
+                db.SaveChanges();    
+
+            }
+        }
+    }
+}
